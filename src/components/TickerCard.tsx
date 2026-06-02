72d34207b48e87d1b435a468f0805799e834c7c1
Heritage Isaac <dipedeji@gmail.com>
2026-06-02 23:15:30 +0100

    feat: change object-fit to fill to show complete unchopped images for cards 7 and 1

--- src/components/TickerCard.tsx
+++ src/components/TickerCard.tsx
@@ -62,7 +62,7 @@ export default function TickerCard({
             style={{
               width: '100%',
               height: '100%',
-              objectFit: 'cover',
+              objectFit: 'fill',
               display: 'block',
               pointerEvents: 'none',
             }}
